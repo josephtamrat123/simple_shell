74d17b1bc9dcba7048189c0203ac4e986c1640f9
josephtamrat123 <141209846+josephtamrat123@users.noreply.github.com>
2023-11-15 22:54:59 +0300

    Create _write_history.c

---
+++ _write_history.c
@@ -0,0 +1,28 @@
+#include "shell.h"
+
+
+void write_history(void)
+{
+	list_t *curr;
+	int fd;
+	char *home;
+
+	home = _getenv("HOME");
+	if (home == NULL)
+		home = "/";
+
+	if (chdir(home) == -1)
+		return;
+
+	fd = open(".simple_shell_history", O_CREAT | O_RDWR | O_APPEND, 0666);
+	if (fd == -1)
+		return;
+
+	for (curr = *get_history_addrss(); curr != NULL; curr = curr->next)
+	{
+		write(fd, curr->str, curr->len);
+		write(fd, "\n", 1);
+	}
+
+	close(fd);
+}
