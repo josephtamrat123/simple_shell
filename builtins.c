c4cb716defcda8663f2f3d84808b46b908fc043a
josephtamrat123 <141209846+josephtamrat123@users.noreply.github.com>
2023-11-16 17:26:49 +0300

    Update builtins.c

--- builtins.c
+++ builtins.c
@@ -1,8 +1,4 @@
 #include "shell.h"
-
-/**
- * env - Prints the environment variables to stdout
-*/
 void env(void)
 {
 	int i;
@@ -13,50 +9,37 @@ void env(void)
 		write(STDOUT_FILENO, env[i], _strlen(env[i]));
 		write(STDOUT_FILENO, "\n", 1);
 	}
-
 	set_process_exit_code(0);
 }
-
 /**
- * _setenv - Sets or adds an environment variable
- * @name: Name for the new env variable
+  * @name: Name for the new env variable
  * @value: Value for the new env variable
- *
- * Return: 1 on success, -1 on error
- */
+  */
 int _setenv(char *name, char *value)
 {
 	int env_index, new_var_len;
 
 	if (validate_env_name(name) == -1)
 		return (-1);
-
 	env_index = get_env_index(name);
 	if (env_index == -1)
 	{/* var doen't exist, SO CREATE IT */
 		int env_count = 0;
 		int old_size, new_size;
-
 		while (__environ[env_count] != NULL)
 			env_count++;
-
 		old_size = sizeof(char *) * (env_count);
 		new_size = sizeof(char *) * (env_count + 2);
 		__environ = _realloc(__environ, old_size, new_size);
 		if (__environ == NULL)
 			dispatch_error("Error while _reallocating memory for new env var");
-
-		/* The new value will be stored at index env_count */
-		env_index = env_count;
-		/* last value For the new env var needs to be NULL */
-		__environ[env_count + 1] = NULL;
+ 		env_index = env_count;
+ 		__environ[env_count + 1] = NULL;
 	}
 	else
 	{
-		/* var exists, so overwrite it's value */
-		free(__environ[env_index]);
+ 		free(__environ[env_index]);
 	}
-
 	new_var_len = _strlen(name) + _strlen(value) + 2;
 	/* store the env var either if it exists or it needs to be overwritten */
 	__environ[env_index] = allocate_memory(sizeof(char) * new_var_len);
@@ -67,13 +50,10 @@ int _setenv(char *name, char *value)
 	set_process_exit_code(0);
 	return (1);
 }
-
 /**
- * _unsetenv - Removes an evironment variable
- * @name: Name for the new env variable
+  * @name: Name for the new env variable
  *
- * Return: 1 on success, -1 on error
- */
+  */
 int _unsetenv(char *name)
 {
 	int env_index, i;
@@ -89,18 +69,12 @@ int _unsetenv(char *name)
 		set_process_exit_code(0);
 		return (1);
 	}
-
-	/* Var doesn't exist, we can print error or do nothing */
-	set_process_exit_code(0); /* Indicates that no error ocurred */
-
+	set_process_exit_code(0); 
 	return (1);
 }
-
 /**
- * _cd - Changes the current directory of the process
- * @path: Path to wich change the working directory
+  * @path: Path to wich change the working directory
  *
- * Return: 1 on success, -1 on error
 */
 int _cd(char *path)
 {
@@ -110,14 +84,12 @@ int _cd(char *path)
 
 	if (_strcmp(path, "-") == 0)
 		path = _getenv("OLDPWD");
-
 	if (path == NULL)
 	{
 		print_builtin_error("cd: OLDPWD not set", "");
 		return (-1);
 	}
-	/* Needed to avoid reading on freed memory */
-	path = duplicate_string(path);
+ 	path = duplicate_string(path);
 	/* store this dir in case of update */
 	oldpwd = getcwd(buff, 1024);
 	if (oldpwd == NULL)
@@ -134,28 +106,22 @@ int _cd(char *path)
 		set_process_exit_code(1);
 		return (-1);
 	}
-	/* Update env variables */
-	_setenv("OLDPWD", oldpwd);
+ 	_setenv("OLDPWD", oldpwd);
 	_setenv("PWD", path);
 	free(path);
 	set_process_exit_code(0);
 	return (1);
 }
-
 /**
- * _alias - Sets an alias command
- * @commands: List of commands
+  * @commands: List of commands
  *
- * Return: -1 on error, 0 otherwise
-*/
+ */
 int _alias(char **commands)
 {
 	int status = 0;
 	list_t *curr;
 	list_t *out_head = NULL;
 	list_t **alias_addrs = get_alias_head();
-
-	/* the alias args starts from position 1 */
 	if (commands[1] == NULL)
 	{ /* This means to list all the aliases */
 		for (curr = *alias_addrs; curr != NULL; curr = curr->next)
@@ -166,15 +132,13 @@ int _alias(char **commands)
 		set_process_exit_code(0);
 		return (1);
 	}
-	/* List aliases and sets the aliases that have the form name=value */
-	status = handle_alias_args(commands, &out_head);
+ 	status = handle_alias_args(commands, &out_head);
 	/* print listed alias */
 	for (curr = out_head; curr != NULL; curr = curr->next)
 	{
 		_puts(curr->str);
 		_puts("\n");
 	}
-	/* free list */
-	free_list(out_head);
+ 	free_list(out_head);
 	return (status);
 }
